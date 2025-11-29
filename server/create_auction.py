e6d4674920d2ca9a6969d2f700abaab72f70bcc2
Anpa <anparasan.anpukkody02@etud.univ-paris8.fr>
2025-11-29 15:54:26 +0100

    FastApi APP defined. Create_auction route defined. Database path defined. HTTPBearer for token. Title, description, price and timestamp are checked. get_current_user and get_user_by_token are defined.

--- server/create_auction.py
+++ server/create_auction.py
@@ -1,6 +1,33 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Form, UploadFile, Depends, File
+from fastapi.security import HTTPBearer
+from fastapi.responses import JSONResponse
 import re
 import time
+import aiosqlite
+
+
+app = FastAPI(title="Secured Auction")
+
+dbPath = "database/database1.db"
+
+security = HTTPBearer(auto_error=False)
+
+
+async def get_user_by_token(token, db):
+    sqlRequest = "select * FROM userinfo WHERE sessionId = ?"
+    cursor = await db.execute(sqlRequest, (token, ))
+    userInfo = await cursor.fetchone()
+    return userInfo
+
+async def get_current_user(credentials = Depends(security)):
+    if credentials is None:
+        errorMessage(401, 13, "User not identified")
+    token = credentials.credentials
+    async with aiosqlite.connect(dbPath) as db: 
+        user = await get_user_by_token(token, db)
+        if not user:
+            errorMessage(401, 13, "User not identified")
+        return user[3]
 
 def errorMessage(status_code: int, code: int, message: str):
     raise HTTPException(
@@ -25,3 +52,25 @@ def check_timestamp(timestamp: int) -> bool:
 
 def check_price(price: float) -> bool:
     return (price >= 5.0)
+
+
+@app.post("/create-auction")
+async def create_auction(
+    title: str = Form(...),
+    description: str = Form(...),
+    price: float = Form(...),
+    timestamp: int = Form(...),
+    image: UploadFile = File(...),
+    current_user = Depends(get_current_user)
+) :
+    if not check_title(title):
+        return errorMessage(400, 32, "Title is not ok")
+
+    if not check_description(description):
+        return errorMessage(400, 38, "Description is not ok")
+
+    if not check_price(price):
+        return errorMessage(400, 31, "Price is not ok")
+
+    if not check_timestamp(timestamp):
+        return errorMessage(400, 30, "Time is not ok")
