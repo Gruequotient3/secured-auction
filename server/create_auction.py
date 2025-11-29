717130c108271dd205060291210f607335fdb37c
Anpa <anparasan.anpukkody02@etud.univ-paris8.fr>
2025-11-29 15:48:16 +0100

    Basic check functions added

---
+++ server/create_auction.py
@@ -0,0 +1,18 @@
+
+import re
+import time
+
+
+def check_title(title: str) -> bool:
+    return (len(title) > 3 and re.fullmatch(r"[A-Za-zÀ-ÖØ-öø-ÿ0-9 ]*", title, flags=re.UNICODE) and len(title) < 15)
+
+def check_description(description: str) -> bool:
+    return (len(description) > 3 and re.fullmatch(r"[\w\s\-',.]*", description, flags=re.UNICODE) and len(description) < 80)
+
+def check_timestamp(timestamp: int) -> bool:
+    now = int(time.time())
+    duration = timestamp - now
+    return (duration >= 120 and duration <= 60*60*24 and timestamp >= now)
+
+def check_price(price: float) -> bool:
+    return (price >= 5.0)
