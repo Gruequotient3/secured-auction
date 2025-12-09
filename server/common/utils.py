3b71a6da2df8e6b2647fd690117ef743148bc50b
Anpa <anparasan.anpukkody02@etud.univ-paris8.fr>
2025-12-09 23:23:13 +0100

    Validate functions rewriten

--- server/common/utils.py
+++ server/common/utils.py
@@ -16,6 +16,9 @@ import os
 import re
 import time
 
+# Internals
+from services.users import Users
+
 # Externals
 import aiosqlite
 
@@ -42,7 +45,32 @@ def check_price(price: float) -> bool:
     return (price >= 5.0)
 
 
+def check_balance(amount):
+    if(amount < 0.0):
+        return False
+    return True
+
+
 def check_timestamp(timestamp: int) -> bool:
     now = int(time.time())
     duration = timestamp - now
     return (duration >= 120 and duration <= 60*60*24 and timestamp >= now)
+
+
+def validate_password(password):
+    if len(password) < 6:
+        return errorMessage(400, 24, "Le mot de passe doit faire au moins six caractères")
+    elif len(password) > 32:
+        return errorMessage(400, 24, "Le mot de passe doit faire moins de trente deux caractères")
+    else:
+        return True
+
+
+async def validate_username(User):
+    if len(User) < 3:
+        return errorMessage(400, 11, "Le pseudo doit faire au moins trois caractères")
+    elif len(User) > 25:
+        return errorMessage(400, 11, "Le pseudo doit faire moins de vingt cinq caractères")
+    if await Users.user_exists(User):
+        return errorMessage(400, 23, "Ce pseudo est déjà utilisé")
+    return True
